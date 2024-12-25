18c7c43e2eec5f0063e86f2562636625f7177888
injoyai <1113655791@qq.com>
2024-12-25 16:56:29 +0800

    优化

--- timer/build.sh
+++ timer/build.sh
@@ -2,7 +2,7 @@ name="timer"
 GOOS=windows GOARCH=amd64 go build -v -ldflags="-H windowsgui -w -s" -o ./$name.exe
 echo "Windows编译完成..."
 echo "开始压缩..."
-upx -9 -k "./$name.exe"
+# upx -9 -k "./$name.exe"
 if [ -f "./$name.ex~" ]; then
   rm "./$name.ex~"
 fi
