fb4147c029bf1d0ba10df60f122565d06cb787bc
钱纯净 <1113655791@qq.com>
2024-12-26 18:48:17 +0800

    101

--- forward/build_win.sh
+++ forward/build_win.sh
@@ -1,16 +1,16 @@
 name="forward"
-GOOS=windows GOARCH=amd64 go build -v -ldflags="-H windowsgui -w -s" -o ./$name.exe
+GOOS=windows GOARCH=amd64 go build -v -ldflags="-H windowsgui -w -s" -o ./bin/$name.exe
 echo "$name 编译完成..."
 echo "开始压缩..."
-upx -9 -k "./$name.exe"
-if [ -f "./$name.ex~" ]; then
-  rm "./$name.ex~"
+upx -9 -k "./bin/$name.exe"
+if [ -f "./bin/$name.ex~" ]; then
+  rm "./bin/$name.ex~"
 fi
-if [ -f "./$name.000" ]; then
-  rm "./$name.000"
+if [ -f "./bin/$name.000" ]; then
+  rm "./bin/$name.000"
 fi
 
 echo "上传至minio"
-cmd.exe /c "in upload minio ./$name.exe"
+cmd.exe /c "in upload minio ./bin/$name.exe"
 
-sleep 2
+sleep 8
