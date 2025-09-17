6145ddc8f4a75c85daff62fedbc71b02e7f9cf35
injoyai <1113655791@qq.com>
2025-09-17 08:35:44 +0800

    "version": "v2.3", "desc": "配合in工具修改名字为i"

--- server/build_win.sh
+++ server/build_win.sh
@@ -2,7 +2,7 @@ name="i_server"
 GOOS=windows GOARCH=amd64 go build -v -ldflags="-H windowsgui -w -s" -o ./$name.exe
 echo "$name 编译完成..."
 echo "开始压缩..."
-#upx -9 -k "./$name.exe"
+upx -9 -k "./$name.exe"
 if [ -f "./$name.ex~" ]; then
   rm "./$name.ex~"
 fi
